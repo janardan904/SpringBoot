311ff8b6f8d1091acfa2c90eaaf83ba9e942c3dd
Dev1 <Dev1@gmail.com>
2019-06-27 01:29:03 +0530

    Data jpa findAll using pegeable Example

---
+++ SpringBootMySqlDataJpaFindAllUsingPageabaleExmp/src/main/java/com/app/runner/ProductRunner.java
@@ -0,0 +1,36 @@
+package com.app.runner;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Component;
+
+import com.app.model.Product;
+import com.app.repo.ProductRepository;
+@Component
+public class ProductRunner implements CommandLineRunner {
+	@Autowired
+	private ProductRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		repo.save(new Product(100,"Ankit" , 3.4));
+		repo.save(new Product(101,"Geeta" , 3.2));
+		repo.save(new Product(102,"Ravi" , 5.3));
+		repo.save(new Product(103,"pam",2.4));
+		System.out.println(repo.getClass().getName());
+		
+	List<Product>list=repo.findAll();
+			list.forEach(System.out::println);
+			
+			PageRequest p=PageRequest.of(3, 1);
+			repo.findAll(p).forEach(System.out::println);
+			
+		
+	}
+
+}
